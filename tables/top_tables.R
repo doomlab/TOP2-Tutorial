907ce84209d53746ea00597b3cc752df3a43e4b5
The Doom Lab <buchananlab@gmail.com>
2026-05-05 10:52:12 -0500

    add tables and start of manuscript

---
+++ tables/top_tables.R
@@ -0,0 +1,3 @@
+top_2015 <- read.delim("tables/top_2015.tsv", stringsAsFactors = FALSE, check.names = FALSE, fileEncoding = "UTF-8")
+top_2025 <- read.delim("tables/top_2025.tsv", stringsAsFactors = FALSE, check.names = FALSE, fileEncoding = "UTF-8")
+top_comparison <- read.delim("tables/top_comparison.tsv", stringsAsFactors = FALSE, check.names = FALSE, fileEncoding = "UTF-8")
